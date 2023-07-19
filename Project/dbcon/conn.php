d64f17802dafcd4eae4d98deece9228fe79ff018
wendigo <saadmansakib275@gmail.com>
2023-07-19 16:26:44 +0600

    minor changes needed. database not updated

--- Project/dbcon/conn.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $conn = mysqli_connect('localhost', 'cmss', '1234', 'car_management_system');
-	if(!$conn){
-		echo 'Connection error: '. mysqli_connect_error();
-	}
-?>
--- Project/odash_index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION["name"]);
-    unset($_SESSION["number"]);
-    unset($_SESSION["car"]);
-    header("Location:landing.php");
-?>
